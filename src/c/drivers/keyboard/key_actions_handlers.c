121f280b84cae6a434237e59eef6eb946c3b0c08
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2024-10-22 12:46:17 +0300

    init: base key actions handlers

---
+++ src/c/drivers/keyboard/key_actions_handlers.c
@@ -0,0 +1,37 @@
+#include "key_actions_handlers.h"
+#include "../../kernel/kernel.h"
+#include "../vga/vga.h"
+#include "../bash/bash.h"
+#include "../file_system/file_contents_input.h"
+
+bool acceptingFileContents = false;
+
+void key_Enter_Action() {
+    start_next_line();
+    handle_command();
+    if (acceptingFileContents) {
+        char_counter = 0;
+        update_pointer_position(0);
+        return;
+    }
+    if (char_counter != 3 && line_counter != 1) out_message_cmd_start();
+}
+
+void key_Backspace_Action() {
+    if (char_counter < 4 && !acceptingFileContents) return;
+    current_address -= 2;
+    char_counter--;
+    update_framebuffer_pointer();
+    out_char((struct FramebufferChar){'\0', DEF_BG, DEF_FG, '\0'});
+    update_pointer_position(0);
+    rem_last_elem();
+}
+
+void key_Escape_Action() {
+    if (acceptingFileContents) {
+        char_counter = 0;
+        update_pointer_position(0);
+        acceptingFileContents = false;
+        out_message_cmd_start();
+    }
+}
---
+++ src/c/drivers/keyboard/key_actions_handlers.h
@@ -0,0 +1,11 @@
+#ifndef KEYHANDLERS_H
+#define KEYHANDLERS_H
+#include "../../kernel/kernel.h"
+
+extern bool acceptingFileContents;
+
+extern void key_Enter_Action();
+extern void key_Backspace_Action();
+extern void key_Escape_Action();
+
+#endif //KEYHANDLERS_H
